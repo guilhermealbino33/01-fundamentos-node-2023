4ca91a5e39ce04b2fc72b46a8ee4ca33abf3ec57
Guilherme Albino <guilhermealbino33@gmail.com>
2023-03-31 22:37:42 -0300

    feat: add get and post

--- src/server.js
+++ src/server.js
@@ -2,9 +2,25 @@ import http from "node:http";
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
+  const users = [];
 
   console.log(method, url);
-  return res.end("Hello");
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "john_doe@email.com",
+    });
+  }
+
+  return res.writeHead(404).end("Not found");
 });
 
 server.listen(3333);
