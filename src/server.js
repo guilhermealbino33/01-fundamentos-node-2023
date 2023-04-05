8f05b43c622584c4868ad7f9a81d2e4a531d56ff
Guilherme Albino <guilhermealbino33@gmail.com>
2023-04-04 21:23:02 -0300

    feat: node streams

--- src/server.js
+++ src/server.js
@@ -1,23 +1,25 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
-const server = http.createServer((req, res) => {
+const users = [];
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-  const users = [];
 
-  console.log(method, url);
+  await json(req, res);
 
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "john_doe@email.com",
+      name,
+      email,
     });
+
+    return res.writeHead(201).end();
   }
 
   return res.writeHead(404).end("Not found");
