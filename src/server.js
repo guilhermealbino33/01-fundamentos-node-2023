b7d2eb183c8b61b280ee37d90322072a4e5dcdf2
Guilherme Albino <guilhermealbino33@gmail.com>
2023-04-07 21:10:28 -0300

    feat: implements query search

--- src/server.js
+++ src/server.js
@@ -15,7 +15,6 @@ const server = http.createServer(async (req, res) => {
   if (route) {
     const routeParams = req.url.match(route.path);
     const { query, ...params } = routeParams.groups;
-
     req.params = params;
     req.query = query ? extractQueryParams(query) : {};
 
